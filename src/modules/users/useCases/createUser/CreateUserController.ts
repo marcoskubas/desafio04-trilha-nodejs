3633a4c699fd7d91762bfaf3c0c337eaa880e058
Marcos Kubaszewski <marcosarobed@gmail.com>
2021-07-17 09:44:33 -0300

    Rota de Criação de Usuário

--- src/modules/users/useCases/createUser/CreateUserController.ts
+++ src/modules/users/useCases/createUser/CreateUserController.ts
@@ -6,7 +6,13 @@ class CreateUserController {
   constructor(private createUserUseCase: CreateUserUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const {name, email} = request.body;
+    try {
+      this.createUserUseCase.execute({email, name});
+    }catch (e){
+      return response.status(400).json({error: e.toString()});
+    }
+    return response.status(201).send();
   }
 }
 
